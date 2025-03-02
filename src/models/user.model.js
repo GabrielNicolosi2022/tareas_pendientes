5709d3ed2f6955b8882dd9dd0e82e92246376cf8
Gabriel Nicolosi <gabianp@live.com.ar>
2025-03-02 19:25:37 -0300

    I create user model

---
+++ src/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userCollection = "Users";
+
+const userSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const userModel = mongoose.model(userCollection, userSchema);
+
+export default userModel;
