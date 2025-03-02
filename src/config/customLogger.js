60dee8890a2143244153b19f2d538d22be5c7b3d
Gabriel Nicolosi <gabianp@live.com.ar>
2025-03-02 17:18:56 -0300

    I create an custom logger with winston dependency and log.utils.js file for her implement. Also an error.log register file.

---
+++ src/config/customLogger.js
@@ -0,0 +1,54 @@
+import winston from "winston";
+
+const customLevelOptions = {
+  levels: {
+    fatal: 0,
+    error: 1,
+    warn: 2,
+    info: 3,
+    http: 4,
+    debug: 5,
+  },
+  colors: {
+    fatal: "red",
+    error: "magenta",
+    warn: "yellow",
+    info: "blue",
+    http: "cyan",
+    debug: "white",
+  },
+};
+
+export const devLog = winston.createLogger({
+  levels: customLevelOptions.levels,
+  transports: [
+    new winston.transports.Console({
+      level: "debug",
+      format: winston.format.combine(
+        winston.format.colorize({ colors: customLevelOptions.colors }),
+        winston.format.simple()
+      ),
+    }),
+  ],
+});
+
+export const prodLog = winston.createLogger({
+  levels: customLevelOptions.levels,
+  transports: [
+    new winston.transports.Console({
+      level: "info",
+      format: winston.format.combine(
+        winston.format.colorize({ colors: customLevelOptions.colors }),
+        winston.format.simple()
+      ),
+    }),
+    new winston.transports.File({
+      level: "error",
+      filename: "logs/errors.log",
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.simple()
+      ),
+    }),
+  ],
+});
