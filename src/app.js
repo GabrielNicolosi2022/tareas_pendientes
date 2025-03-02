ce4ff33611b875873b0403dfd2afa5c596c999ed
Gabriel Nicolosi <gabianp@live.com.ar>
2025-03-02 09:16:36 -0300

    upload files

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
+import cors from "cors";
+import __dirname from "./dirname";
 
+/* CONFIGURATIONS */
 const app = express();
+const PORT = 8080;
+
+/* Express */
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(express.static(`${__dirname}/public`));
+app.use(cors());
