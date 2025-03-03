89efd5e815f69a7d0b5b708cf67f2c956146ce8e
Gabriel Nicolosi <gabianp@live.com.ar>
2025-03-03 16:32:08 -0300

    I create user dto

--- src/dto/user.dto.js
+++ src/dto/user.dto.js
@@ -1,9 +1,9 @@
-export const formatUserData = (data) => ({
+export const formatedUser = (data, hashedPassword) => ({
   first_name: data.first_name,
   last_name: data.last_name,
   username: data.username,
   email: data.email,
   phone: data.phone,
   role: data.role,
-  password: data.password, //? quizá hashedPassword
+  password: hashedPassword,
 });
